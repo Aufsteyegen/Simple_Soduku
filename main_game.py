8bc4c104be2076b63225bb06884fb19048fed47f
Gregory Caesar <main@Gregorys-MacBook-Pro.local>
2023-01-12 11:39:15 -0600

    first commit

---
+++ main_game.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from play_logic import *
+
+
+# main window
+window = tk.Tk()
+window.geometry("630x630")
+window.title("Simple Sudoku")
+
+#player data
+wins = 0
+plays = 0
+fastest_win = 0
+
+#widgets
+stats = ttk.Label(text=f"Wins: {wins} | Plays: {plays} | Fastest Win: {fastest_win}",
+                  font=('Arial', 20))
+
+# stats.pack(pady=20)
+
+def doSomething(event):
+
+
+    print(event.char)
+
+class SudBlock:
+    def __init__(self, start_row, start_column):
+        self.start_row = start_row
+        self.start_column = start_column
+
+    def new_block(self):
+        entries = {}
+        cur_row, cur_column = self.start_row, self.start_column
+        for i in range(1, 10):
+            entry = tk.Entry(bg="white", width=2, justify='center', font=('Arial', 40))
+            entry.bind(i, doSomething)
+            entries[(cur_row, cur_column)] = entry.grid(row=cur_row, column=cur_column)
+            if i % 3 == 0:
+                cur_row += 1
+                cur_column = self.start_column
+            else:
+                cur_column += 1
+        return entries
+
+SudBlock(1, 1).new_block()
+
+
+
+#adds widgets to window
+
+
+#events
+
+
+
+
+
+
+
+# runs tkinter event loop
+window.mainloop()
+
