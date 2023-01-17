d6a887d6b10302b4d199d45acdd149da0d02189a
Gregory Caesar <main@Gregorys-MacBook-Pro.local>
2023-01-17 00:29:42 -0600

    first iteration of grid; adds entry validation to sublock class

--- main_game.py
+++ main_game.py
@@ -15,33 +15,43 @@ fastest_win = 0
 
 #widgets
 stats = ttk.Label(text=f"Wins: {wins} | Plays: {plays} | Fastest Win: {fastest_win}",
-                  font=('Arial', 20))
+                  font=('Roboto', 20))
 
 # stats.pack(pady=20)
 
-
-def validate_entry(entry):
-    var = entry.get()
-    print(var + '1')
-
-def doSomething(event):
-    print(event.char)
-
-
+def testVal(entry_val, action_type, idx):
+    if idx != '0':
+        return False
+    if action_type == '1':
+        if not entry_val.isdigit() or int(entry_val) == 0:
+            return False
+    return True
 
 class SudBlock:
     def __init__(self, start_row, start_column):
         self.start_row = start_row
         self.start_column = start_column
 
+    def next_widget(self, event):
+        if event.keysym == 'd':
+            event.widget.tk_focusNext().focus()
+        elif event.keysym == 'a':
+            event.widget.tk_focusPrev().focus()
+        elif event.keysym == 's':
+            event.widget.tk_focusNext().tk_focusNext().tk_focusNext().focus()
+        else:
+            event.widget.tk_focusPrev().tk_focusPrev().tk_focusPrev().focus()
+
     def new_block(self):
         entries = {}
         cur_row, cur_column = self.start_row, self.start_column
         for i in range(1, 10):
             txt_var = tk.StringVar()
-            entry = tk.Entry(validatecommand=validate_entry(txt_var), validate='key', bg="white", width=2,
-                             justify='center', textvariable=txt_var, font=('Arial', 40))
-            entry.bind("<Key>", doSomething)
+            entry = tk.Entry(validate='key', bg="white", width=2, justify='center',
+                             textvariable=txt_var, font=('Roboto', 40))
+            entry['validatecommand'] = (entry.register(testVal), '%P', '%d', '%i')
+            entry.bind("<d>", self.next_widget), entry.bind("<a>", self.next_widget)
+            entry.bind("<s>", self.next_widget), entry.bind("<w>", self.next_widget)
             entries[(cur_row, cur_column)] = entry.grid(row=cur_row, column=cur_column)
             if i % 3 == 0:
                 cur_row += 1
@@ -50,7 +60,11 @@ class SudBlock:
                 cur_column += 1
         return entries
 
-SudBlock(1, 1).new_block()
+SudBlock(1, 1).new_block(), SudBlock(1, 4).new_block()
+SudBlock(1, 7).new_block(), SudBlock(4, 1).new_block()
+SudBlock(4, 4).new_block(), SudBlock(4, 7).new_block()
+SudBlock(7, 1).new_block(), SudBlock(7, 4).new_block()
+SudBlock(7, 7).new_block()
 
 
 
